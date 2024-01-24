8869e4a297cae07a53f4c36c9c198194fbbf1cfb
Zane <kuzunoha.zein@gmail.com>
2024-01-24 19:02:08 +1300

    Moved project to Java for ease of use with the GUI toolset.

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    
+    // The main method will execute when our program is run
+    public static void main(String[] args) {
+      
+        // We use System.out.println to print a line of text to the screen
+        System.out.println("Hello, World!");
+    }
+}
